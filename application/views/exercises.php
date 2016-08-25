236584cdec14e081b5bd626321bdd2e88fc0e142
Kayvon Fatahalian <kayvonf@gmail.com>
2016-08-24 20:37:01 -0400

    Stubs.

--- application/views/exercises.php
+++ application/views/exercises.php
@@ -3,7 +3,6 @@
 
 <div class="home_title">Parallel Computer Architecture and Programming <span style="font-size: 12pt;">(CMU 15-418/618)</span> </div>
 
-
 <p>This page contains practice exercises to help you understand
 material in the course. Reference material and lecture videos are
 available on the <a href="<?php echo site_url('lectures'); ?>">Lectures</a> page.</p>
@@ -11,64 +10,15 @@ available on the <a href="<?php echo site_url('lectures'); ?>">Lectures</a> page
 <div class="overview_main_item overview_ruled_element">Self-Check Exercises</div>
 
 <p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_01.pdf'; ?>">Exercise 1: A Task Queue on a Multi-Core, Multi-Threaded CPU</a></div>
+<div><a href="<?php echo $exercises_base_url . '/dummylink.pdf'; ?>">Exercise 1: Dummy Link</a></div>
 <div class="small_text colored_text">Concepts tested: multi-core architecture, hardware multi-threading</div>
 </p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_02.pdf'; ?>">Exercise 2: Data-Parallel Histograms, and the Prof. Kayvon Processing Unit (PKPU)</a></div>
-<div class="small_text colored_text">Concepts tested: multi-core architecture, hardware multi-threading, data-parallel algorithms, abstraction vs. implementation</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_03.pdf'; ?>">Exercise 3: More on Histograms, Angry Students</a></div>
-<div class="small_text colored_text">Concepts tested: cache coherence, performance optimization</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exam1_practice.pdf'; ?>">Exam 1 Practice Exercises</a></div>
-<div class="small_text colored_text">Concepts tested: everything up to Exam 1</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_04.pdf'; ?>">Exercise 4: Good Business Practices, Understanding Load-linked, Store-conditional</a></div>
-<div class="small_text colored_text">Concepts tested: elastic scheduling, implementing synchronization, review of cache coherence and latency hiding</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_05.pdf'; ?>">Exercise 5: Concurrent Linked Lists and Heaps</a></div>
-<div class="small_text colored_text">Concepts tested: fine-grained locking and transactional memory</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exercise_06.pdf'; ?>">Exercise 6: Justin, Kanye and Taylor Bring Locality Back, Controlling DRAM</a></div>
-<div class="small_text colored_text">Concepts tested: scheduling Spark programs, how DRAM works</div>
-</p>
-
-<p>
-<div><a href="<?php echo $exercises_base_url . '/exam2_practice.pdf'; ?>">Exam 2 Practice Exercises</a></div>
-<div class="small_text colored_text">Concepts tested: everything from the memory consistency lecture onward</div>
-</p>
-
 
 <div class="overview_main_item overview_ruled_element">Programming Assignments</div>
 
 <p>
-<div><a href="<?php echo article_url('3'); ?>">Assignment 1: Analyzing Parallel Program Performance on a Quad-Core CPU</a></div>
+<div><a href="<?php echo article_url('3'); ?>">Assignment 1: Dummy Link</a></div>
 </p>
-
-<p>
-<div><a href="<?php echo article_url('4'); ?>">Assignment 2: A Simple Renderer in CUDA</a></div>
-</p>
-
-<p>
-<div><a href="<?php echo article_url('9'); ?>">Assignment 3: ParaGraph: A Parallel Graph Library on Xeon Phi</a></div>
-</p>
-
-<p>
-<div><a href="<?php echo article_url('7'); ?>">Assignment 4: A Simple, Elastic Web Server</a></div>
-</p>
-
 
 <p>&nbsp;</p>
 
