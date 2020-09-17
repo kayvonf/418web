cda0cfef697860643d2a7492b16d8f968cda0dcc
Fait Poms <faitpoms@gmail.com>
2020-09-17 11:30:18 -0700

    Add support for markdown comments

--- pyweb418/lectures/templatetags/message_tags.py
+++ pyweb418/lectures/templatetags/message_tags.py
@@ -1,3 +1,4 @@
+import markdown
 # Use datetime if not localizing timezones
 import datetime
 # Otherwise use timezone
@@ -10,3 +11,15 @@ register = template.Library()
 @register.filter
 def days_ago(time, days):
     return time + datetime.timedelta(days=days) < timezone.now() # or timezone.now() if your time is offset-aware
+
+
+@register.filter
+def markdownify(text):
+    # safe_mode governs how the function handles raw HTML
+    return markdown.markdown(text, safe_mode='escape',
+                             extensions=['pymdownx.pathconverter'],
+                             extension_configs={
+                                 "pymdownx.pathconverter": {
+                                     'base_path': '//',
+                                     "absolute": True
+                                 }})
